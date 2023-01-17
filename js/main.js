34c9472b409c7644287837dff0c9b9b5a472b621
ashraful90 <ashrafulislam050643@gmail.com>
2023-01-17 12:50:51 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').bxSlider();
+    $('.main_nav').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth:991,
+        
+       
+    });
+  });
+
